cbae22f9d0a568203394b6cf46022acd5f4800ce
samuwyat989 <samuwyat989@ed.amdsb.ca>
2016-10-17 04:43:24 -0700

    Add project files.

---
+++ collatzConjecture/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace collatzConjecture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputButton = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputButton
+            // 
+            this.inputButton.Location = new System.Drawing.Point(191, 40);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(90, 20);
+            this.inputButton.TabIndex = 0;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(197, 66);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 1;
+            this.start.Text = "Calculate";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 503);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.inputButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputButton;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ collatzConjecture/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace collatzConjecture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            Graphics col = this.CreateGraphics();
+            SolidBrush brush = new SolidBrush(Color.Black);
+            Font font = new Font("TimeNewRoman", 9);
+
+            
+            col.DrawString("INPUT NUMBER:" + inputButton.Text, font, brush, 20, 20);
+            int loopNumber = 100;
+            int x = Convert.ToInt32(inputButton.Text);
+            int y = 50;
+
+            for (int i = 0; i < loopNumber; i++)
+            {
+                if (x % 2 == 0)
+                {
+                    x = x / 2;
+                    col.DrawString(Convert.ToString(x), font, brush, 20, y);
+                    y = y + 20;
+                    Thread.Sleep(1000);
+                }
+
+                else if (x == 1) 
+                {
+                    Thread.Sleep(3000);
+                    col.Clear(Color.White);
+                    break;  
+                } 
+
+                else
+                {
+                    x = 3 * x + 1;
+                    col.DrawString(Convert.ToString(x), font, brush, 20, y);
+                    y = y + 20;
+                    Thread.Sleep(1000);
+                }
+            }
+
+
+            
+        }
+    }
+}
